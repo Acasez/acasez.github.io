0771409ad14d6a8e71bab3576cd3cb3ff714ceac
Acasez <edvin.sanne@gmail.com>
2026-08-15 15:20:32 +0200

    Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import MainHeader from "./components/MainHeader";
 import PortfolioIndex from "./Subpages/PortfolioIndex";
-import NestedTooltips from "./Subpages/NestedTooltips";
+
+const NestedTooltips = lazy(() => import("./Subpages/NestedTooltips"));
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
-        <MainHeader />
+      <MainHeader />
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<PortfolioIndex />} />
           <Route path="/EdvinsNestedTooltips" element={<NestedTooltips />} />
         </Routes>
-      </BrowserRouter>
+      </Suspense>
     </>
   );
 }
