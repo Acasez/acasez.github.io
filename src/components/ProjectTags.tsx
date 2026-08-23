ee4bc1acf9766bf6cfa036aedb5d5eb507c57beb
Acasez <edvin.sanne@gmail.com>
2026-08-23 14:20:55 +0200

    Simplified Project Tags file

--- src/components/ProjectTags.tsx
+++ src/components/ProjectTags.tsx
@@ -4,31 +4,22 @@ import "../CSS/ProjectTags.css";
 interface TagProps {
   projectTags?: string[];
   selectedTag?: string | null;
-  onTagClick?: (tag: string | null) => void;
 }
 
-export default function ProjectTags({
-  projectTags,
-  selectedTag,
-  onTagClick,
-}: TagProps) {
+export default function ProjectTags({ projectTags, selectedTag }: TagProps) {
   if (!projectTags) return null;
 
   return (
     <div className="project-tags">
       {projectTags.map((tag) => (
-        <button
+        <div
           key={tag}
           className={`tag ${tag.toLowerCase().replace("#", "sharp").replace(" ", "-")} ${
             selectedTag === tag ? "selected" : ""
           }`}
-          onClick={() => {
-            // If clicking the currently selected tag, clear the filter
-            onTagClick?.(selectedTag === tag ? null : tag);
-          }}
         >
-          {tag}
-        </button>
+          {tag.replace("-", " ")}
+        </div>
       ))}
     </div>
   );
