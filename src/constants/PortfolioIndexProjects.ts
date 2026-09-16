71408adcf4d93ce09fabf19f3be756b74975b0bf
Acasez <edvin.sanne@gmail.com>
2026-09-16 12:01:17 +0200

    Updated LMS description slightly

--- src/constants/PortfolioIndexProjects.ts
+++ src/constants/PortfolioIndexProjects.ts
@@ -143,7 +143,7 @@ export const smallProjects = [
     name: "Learning Platform Website",
     subtitle: "Learning platform website",
     description:
-      "Built as the final project on the Lexicon part of the fullstack development course I’m doing. Features a backend with an SQL database, JWT token authentication and a frontend with React and Tailwind. We planned out the project in Figma, set up github workflow actions and used a github project kanban page to track progress. \n\nThe web page has two roles: Teacher and Student. Teachers can manage courses and learning content, while students can access their assigned course, modules, activities, resources, and submissions. I mostly worked in frontend, making react pages and components such as our reusable Data Table which can create a table for a type of T.",
+      "Built as the final project on the Lexicon part of the fullstack development course I’m doing. Features a backend with an SQL database, JWT token authentication and a frontend with React and Tailwind. We planned out the project in Figma, set up github workflow actions and used a github project kanban page to track progress and user stories. \n\nThe web page has two roles: Teacher and Student. Teachers can manage courses and learning content, while students can access their assigned course, modules, activities, resources, and submissions. I mostly worked in frontend, making react pages and components such as our reusable Data Table which can create a sort, filter and pageable table for a type of T.",
     githubLink: "https://github.com/Leo-Mart/Lexicon_Project_Assignment",
     hasSubpage: false,
   },
